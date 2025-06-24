ebd52ed363a9d5400f54d745d42b4e28a9907fa1
Gowrisankar Allam <allamgowrisankar001@gmail.com>
2025-06-24 21:35:31 +0530

    a

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Project info
 
-**URL**: https://lovable.dev/projects/403f1728-c7e9-47c0-93c2-fa4894cdc128
+**URL**: https://lovable.dev/projects/8a6d20df-80b8-4237-a0d8-fbea08c2ac5f
 
 ## How can I edit this code?
 
@@ -10,7 +10,7 @@ There are several ways of editing your application.
 
 **Use Lovable**
 
-Simply visit the [Lovable Project](https://lovable.dev/projects/403f1728-c7e9-47c0-93c2-fa4894cdc128) and start prompting.
+Simply visit the [Lovable Project](https://lovable.dev/projects/8a6d20df-80b8-4237-a0d8-fbea08c2ac5f) and start prompting.
 
 Changes made via Lovable will be committed automatically to this repo.
 
@@ -62,7 +62,7 @@ This project is built with:
 
 ## How can I deploy this project?
 
-Simply open [Lovable](https://lovable.dev/projects/403f1728-c7e9-47c0-93c2-fa4894cdc128) and click on Share -> Publish.
+Simply open [Lovable](https://lovable.dev/projects/8a6d20df-80b8-4237-a0d8-fbea08c2ac5f) and click on Share -> Publish.
 
 ## Can I connect a custom domain to my Lovable project?
 
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,22 +1,37 @@
 
-import React from "react";
-import CourseHeader from "../components/CourseHeader";
-import CourseSidebar from "../components/CourseSidebar";
-import CourseContent from "../components/CourseContent";
-import CourseProgress from "../components/CourseProgress";
+import Header from "../components/Header";
+import ImageGallery from "../components/ImageGallery";
+import ApartmentInfo from "../components/ApartmentInfo";
+import MoveInDetails from "../components/MoveInDetails";
+import AboutProperty from "../components/AboutProperty";
+import PremiumAmenities from "../components/PremiumAmenities";
+import ContactOwner from "../components/ContactOwner";
+import SimilarProperties from "../components/SimilarProperties";
+import Footer from "../components/Footer";
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-white">
-      <CourseHeader />
-      <div className="container mx-auto px-4 py-6 flex flex-col lg:flex-row gap-6">
-        <div className="w-full lg:w-3/4">
-          <CourseSidebar />
-          <CourseContent />
-        </div>
-        <div className="w-full lg:w-1/4">
-          <CourseProgress />
-        </div>
+    <div className="min-h-screen bg-gradient-to-br from-gray-950 via-black to-gray-900 text-white relative overflow-x-hidden">
+      {/* Modern gradient background */}
+      <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-purple-900/20 via-black to-pink-900/10"></div>
+      <div className="absolute inset-0 bg-[radial-gradient(circle_at_bottom_right,_var(--tw-gradient-stops))] from-pink-500/5 via-transparent to-violet-500/5"></div>
+      
+      <div className="relative z-10">
+        <Header />
+        <main className="pb-24 px-4 max-w-md mx-auto lg:max-w-2xl space-y-6">
+          <ImageGallery />
+          <div className="space-y-6">
+            <ApartmentInfo />
+            <div className="grid gap-6">
+              <MoveInDetails />
+              <AboutProperty />
+            </div>
+            <PremiumAmenities />
+            <ContactOwner />
+            <SimilarProperties />
+          </div>
+        </main>
+        <Footer />
       </div>
     </div>
   );
